b85643b2ff8554ec5e1c4ce5d77b62ffb2618ec2
guto_22000@hotmail.com <guto_22000@hotmail.com>
2017-02-23 17:18:23 -0300

    SIGImp Project. Initial skeleton build.

---
+++ lang/en/sigimp.php
@@ -0,0 +1,42 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ * English strings for sigimp
+ *
+ * You can have a rather longer description of the file as well,
+ * if you like, and it can span multiple lines.
+ *
+ * @package    mod_sigimp
+ * @copyright  2016 Your Name <your@email.address>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['modulename'] = 'sigimp';
+$string['modulenameplural'] = 'sigimps';
+$string['modulename_help'] = 'Use the sigimp module for... | The sigimp module allows...';
+$string['sigimp:addinstance'] = 'Add a new sigimp';
+$string['sigimp:submit'] = 'Submit sigimp';
+$string['sigimp:view'] = 'View sigimp';
+$string['sigimpfieldset'] = 'Custom example fieldset';
+$string['sigimpname'] = 'sigimp name';
+$string['sigimpname_help'] = 'This is the content of the help tooltip associated with the sigimpname field. Markdown syntax is supported.';
+$string['sigimp'] = 'sigimp';
+$string['pluginadministration'] = 'sigimp administration';
+$string['pluginname'] = 'sigimp';
